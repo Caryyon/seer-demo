6c0c5d4042acfddab5641e593d5608d6c5e2dce4
Cary Wolff <boss@caryyon.com>
2021-08-09 21:12:23 -0500

    feat(ui): adds types for router

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Login from "./components/Login";
 import styled from "styled-components";
 import { Navbar, Alignment, Button } from "@blueprintjs/core";
